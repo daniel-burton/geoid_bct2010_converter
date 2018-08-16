c0db8c78565f0d753a029f5d281ed1399a1380d8
dan burton <daniel.r.burton@gmail.com>
2018-08-15 22:13:42 -0400

    tightened up, removed a lot of extraneous comments

--- geoid_btc2010_convert.py
+++ geoid_btc2010_convert.py
@@ -1,50 +1,33 @@
-import csv, argparse
-
-#PURPOSE: take a CENSUS TRACT census file from american factfinder and append a
-#column with NYC DCP's BCT2010 numbers for better joining with GIS files, takes
-#an 11-digit geoid as input. Originally done with sys.args, now updated to use
-#argparse. Not sure if actually better to use argparse. But def less opaque to me
-
-#usage example:
-#C:\python27\ArcGIS10.3\python.exe C:\Users\d_burton\Desktop\python\geoid_translator.py C:\Users\d_burton\Desktop\censusfile.csv
-#really running over the 80 character limit with these windows file paths ; _ ;
+import csv, argparse, sys
 
 def bct_translate(geoid):
-    """take the 11 digit geoid and turn it into a DCP boro code"""
+    """take the 11 digit geoid and turn it into a DCP boro code using a dictionary"""
     boros = {"061":"1", "005":"2", "047":"3", "081":"4", "085":"5"}
-    #dictionary that associates GEOID county codes to boro codes
     return boros[geoid[3:6]]
 
-parser = argparse.ArgumentParser(description="""Translate 11-digit Census Tract
-                                 GEOID into a NYC-DCP BCT2010 number for better
-                                 joining with GIS or Excel files""")
-#create argument parser "parser," translates command line args into variables
-
-parser.add_argument("input_file", help = """provide the input file, Census Bureau
-                                            data at the tract level""")
-default_output = "bct_translator_output.csv"
-parser.add_argument("output_file", nargs = '?', help = """provide output file, default is
-                                            (input_file)_bct_output.csv""",
-                                            default = default_output)
-#second positional argument is output file, else default value
-
+#create parser, assign command line arguments with defaults for input and output files
+parser = argparse.ArgumentParser(description = ("Translate 11-digit Census Tract GEOID into a"
+                                                " NYC-DCP BCT2010 number for better joining with"
+                                                " GIS or Excel files"))
+parser.add_argument("input_file", help = ("provide the input file, Census Tract data"))
+parser.add_argument("output_file", nargs = '?', help = ("provide output file, default is"
+                                                        " bct_translator_output.csv"),
+                                                        default = "bct_translator_output.csv")
 args = parser.parse_args()
 
+#now create csv reader and writer iterators
 input_reader = csv.reader(open(args.input_file))
-#create reader, an iterable that returns next line every time called
-output_writer = csv.writer(open(args.output_file,'w'),dialect='excel')
+if sys.platform == "win32": #to deal with weird windows csv error
+    output_writer = csv.writer(open(args.output_file,'wb'))
+else:
+    output_writer = csv.writer(open(args.output_file,'w'))
+output_writer.writerow(labels)  #write label row to output
 
-labels = next(input_reader)  #first row in csv is column labels
-labels.append("bct2010")  #add column for bct2010
-output_writer.writerow(labels)  #write row to output
+print(("Creating a copy of {0} with BCT2010 added in last column,
+    "saving as {1}\n").format(args.input_file, args.output_file))
 
-print("""Creating a copy of {0} with BCT2010 added in last column,
-        saving as {1}\n""".format(args.input_file, args.output_file))
-
+#loop through the whole input csv, add the bct2010, output
 for tract in input_reader:
-    #note, will start from second row since first row was read above
-    geoid = tract[1] #2nd column is geoid
+    geoid = tract[1]
     tract.append(bct_translate(geoid) + geoid[-6:])
-    #new column is boro code + last 6 digits of geoid
     output_writer.writerow(tract)
-    #write full row with new column to the output file
